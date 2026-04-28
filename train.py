c44f299ca57f31f112780e8151e4661749335a7b
Sean Ko <seanko138@gmail.com>
2026-04-27 20:02:52 -0700

    change batchsize

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ effective_batch_size = 96
 # gradient_accumulation_steps (micro batches) are now calculated from the two. The main issue is it's possible to construct non-evenly divisible batches so we simply round up
 
 # batch_size = 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
-batch_size = 6 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 4 # if gradient_accumulation_steps > 1, this is the micro-batch size
 gradient_accumulation_steps = int(float(effective_batch_size)/float(batch_size)) # used to simulate larger batch sizes
 remainder = effective_batch_size%batch_size
 if (remainder > 0):
