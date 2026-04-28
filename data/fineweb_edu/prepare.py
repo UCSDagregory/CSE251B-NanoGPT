35f810b2a998d5ea6ecb15f5cd779ba051a29d06
Ravi Gadgil <ravigadgil123@gmail.com>
2026-04-27 23:11:05 -0700

    Change doc size due to disk space

--- data/fineweb_edu/prepare.py
+++ data/fineweb_edu/prepare.py
@@ -16,8 +16,8 @@ import tiktoken
 from datasets import load_dataset
 
 # --- Config ---
-MAX_DOCS = 7_500_000       # ~2B tokens
-VAL_DOCS = 3_750
+MAX_DOCS = 5_000_000       # ~2B tokens
+VAL_DOCS = 2_500
 
 enc = tiktoken.get_encoding("gpt2")
 
