7eeb2144e3391e5f3d71b279b38fa52a5fe5e186
Ravi Gadgil <ravigadgil123@gmail.com>
2026-04-29 09:25:09 -0700

    P2 Test

--- data/fineweb_edu/prepare.py
+++ data/fineweb_edu/prepare.py
@@ -16,8 +16,8 @@ import tiktoken
 from datasets import load_dataset
 
 # --- Config ---
-MAX_DOCS = 5_000_000
-VAL_DOCS = 2_500
+MAX_DOCS = 5_000
+VAL_DOCS = 100
 
 enc = tiktoken.get_encoding("gpt2")
 
