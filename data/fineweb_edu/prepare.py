9dcd8138aeae49935ced96ad85a6b1650afa6990
Ravi Gadgil <ravigadgil123@gmail.com>
2026-04-29 14:32:27 -0700

    Reset prepare/train for P2

--- data/fineweb_edu/prepare.py
+++ data/fineweb_edu/prepare.py
@@ -16,8 +16,8 @@ import tiktoken
 from datasets import load_dataset
 
 # --- Config ---
-MAX_DOCS = 5_000
-VAL_DOCS = 100
+MAX_DOCS = 5_000_000
+VAL_DOCS = 2_500
 
 enc = tiktoken.get_encoding("gpt2")
 
