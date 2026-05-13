b55960f86b5c1aa83cae34afb69f433595e3d91c
Dagin Gregory <dagin.gregory@gmail.com>
2026-05-12 20:09:16 -0700

    fixed params

--- train.py
+++ train.py
@@ -117,7 +117,7 @@ if (not args.stream_config is None):
         raise ValueError("Not allowed to define a stream config when not streaming.")
     streaming_config = args.stream_config
 
-eval_interval = 100 # How many iters until re-calculate val. loss
+eval_interval = 500 # How many iters until re-calculate val. loss
 # eval_interval = 1 # How many iters until re-calculate val. loss
 log_interval = 1
 eval_iters = 25 # How many times to calculate val.loss per 'eval interval'
@@ -128,8 +128,7 @@ iters_per_checkpoint = 50
 max_checkpoints_to_keep = 4 
 
 dataset = args.data_fd_name
-# effective_batch_size = 96
-effective_batch_size = 1
+effective_batch_size = 96
 # Important note
 # batch_size exists to be memory efficient, if there isn't enough space in the GPU memory it will spill over into SMEM (shared memory)
 # which is significantly slower than it just living in VRAM
@@ -144,9 +143,9 @@ effective_batch_size = 1
 # It's been changed s.t. the user now fixes effective_batch_size to a number they deem reasonable for clean gradients and batch_size to ensure training is as performant as possible
 # gradient_accumulation_steps (micro batches) are now calculated from the two. The main issue is it's possible to construct non-evenly divisible batches so we simply round up
 
-# batch_size = 48 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 48 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # batch_size = 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
-batch_size = 1 # if gradient_accumulation_steps > 1, this is the micro-batch size
+# batch_size = 1 # if gradient_accumulation_steps > 1, this is the micro-batch size
 gradient_accumulation_steps = int(float(effective_batch_size)/float(batch_size)) # used to simulate larger batch sizes
 remainder = effective_batch_size%batch_size
 if (remainder > 0):
